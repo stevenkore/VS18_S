a52c54e24f7c63550df002ed2cbb7f9903163468
steven.kore <steven.kore@khk.ee>
2019-02-21 11:06:09 +0200

    tehtud

--- menu/index.php
+++ menu/index.php
@@ -2,140 +2,21 @@
 /**
  * Created by PhpStorm.
  * User: steven.kore
- * Date: 21.01.2019
- * Time: 15:11
+ * Date: 07.02.2019
+ * Time: 9:51
  */
-$menu = array(
-    array(
-        'tyyp' => 'praed',
-        'nimi' => 'Kanafilee',
-        'hind' => 2.60
-    ),
-    array(
-        'tyyp' => 'praed',
-        'nimi' => 'Kanakaste',
-        'hind' => 2.40
-    ),
-    array(
-        'tyyp' => 'supid',
-        'nimi' => 'Borš',
-        'hind' => 1.40
-    ),
-    array(
-        'tyyp' => 'magustoidud',
-        'nimi' => 'Tiramisu',
-        'hind' => 1.20
-    ),
-    array(
-        'tyyp' => 'joogid',
-        'nimi' => 'Morss',
-        'hind' => 0.35
-    ),
-    array(
-        'tyyp' => 'joogid',
-        'nimi' => 'Piim',
-        'hind' => 0.50
-    ),
-    array(
-        'tyyp' => 'joogid',
-        'nimi' => 'Mahl',
-        'hind' => 0.50
-    ),
-    array(
-        'tyyp' => 'supid',
-        'nimi' => 'Kanasupp',
-        'hind' => 1.85
-    ),
-    array(
-        'tyyp' => 'magustoidud',
-        'nimi' => 'Kreembrülee',
-        'hind' => 1.90
-    ),
-);
-function omaHind($ale, $hind)
-{
-    return round(($ale * $hind), 2);
-}
-
-;
-function kuvaMenu($tyyp)
-{
-    global $menu;
-    foreach ($menu as $roog) {
-        if ($roog['tyyp'] === $tyyp) {
-            echo '<li class="list-group-item">
-                    <p>' . $roog['nimi'] . ' ' . '<span class="badge badge-primary">' . $roog['hind'] . '€</span>
-                    <span class="badge badge-success">' . omaHind(0.75, $roog['hind']) . '€' . '</span>
-                    </p>
-                </li>';
-        }
-    }
-}
-
-;
+require_once 'lib/fnk.php';
+require_once 'menus/menu.php';
+// lehe sisu
+// päis - header ja muud vajalikud osad
+pais();
+// menüü elemendid - menu funktsioon kasutab menu_element funktsiooni sisu loomiseks
+menu(array(
+    'praed' => $praed,
+    'supid' => $supid,
+    'magus' => $magusad,
+    'joogid' => $joogid
+));
+// jalus - kõik JS asjad
+jalus();
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>sööklamenüü</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
-</head>
-<body>
-
-<div class="container">
-    <h1>Söökla menüü:</h1>
-    <br><br>
-    <div class="row">
-        <div class="col-8">
-            <div class="card">
-                <h1 class="card-header">PRAED</h1>
-                <ul class="list-group">
-
-                    <?php
-                    kuvaMenu('praed');
-                    ?>
-
-                </ul>
-            </div>
-        </div>
-        <div class="col-8">
-            <div class="card">
-                <h1 class="card-header">SUPID</h1>
-                <ul class="list-group">
-
-                    <?php
-                    kuvaMenu('supid');
-                    ?>
-
-                </ul>
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-8">
-            <div class="card">
-                <h1 class="card-header">MAGUSTOIDUD</h1>
-                <ul class="list-group">
-                    <?php
-                    kuvaMenu('magustoidud');
-                    ?>
-                </ul>
-            </div>
-        </div>
-        <div class="col-8">
-            <div class="card">
-                <h1 class="card-header">JOOGID</h1>
-                <ul class="list-group">
-                    <?php
-                    kuvaMenu('joogid');
-                    ?>
-                </ul>
-            </div>
-        </div>
-    </div>
-</div>
-</body>
-</html>
